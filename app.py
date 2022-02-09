df28d0b8b187c6728c0fa0b1fd0671abf87d493d
anshuman-singh-bhadauria <69234556+anshuman-singh-bhadauria@users.noreply.github.com>
2022-02-09 08:46:27 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_restful import Api, Resource
+import os
+import pathlib
+from db import DB
+from summarizer import text_summarizer
+
+app = Flask(__name__)
+api=Api(app)
+
+database = DB("database.db")
+
+class SubmitDocument(Resource):
+
+    def post(self, document_text):
+        if len(document_text)>0:
+            document_id=database.insert(document_text)
+            database.show_table(limit=5)
+            print(document_id)
+
+class GetDocument(Resource):
+
+    def get(self, document_id):
+        document=database.fetch(document_id)
+        text_summary = text_summarizer(document)
+        return {"Document summary": text_summary}
+
+api.add_resource(SubmitDocument, "/<string:document_text>")
+api.add_resource(GetDocument, "/<int:document_id>")
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 30003))
+    app.run(host='0.0.0.0', port=port, debug=True, use_reloader=True)
